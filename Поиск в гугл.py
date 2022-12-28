09d8a7246e4440ef42ebcaf037a938cb3fdbc078
AJIEXx <dima12041985@gmail.com>
2022-12-28 11:55:12 +0600

    Add File

--- Поиск в гугл.py
+++
@@ -1,9 +0,0 @@
-# Если перейти на google.com является для тебя тяжелой задачей, тебя могут спасти эти 3 строчки кода.
-#
-# Чтобы всё работало как швейцарские часы --> pip install google
-
-from googlesearch import search
-query = "best hentai videos with Asynco and Max Wayld"
-
-for i in search(query, tld="co.in", num=10, stop=10, pause=2):
-    print(i)
