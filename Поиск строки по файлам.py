7341be01f57030eee1ee44a2ed6c5f97af0d72d3
AJIEXx <dima12041985@gmail.com>
2022-12-28 11:26:26 +0600

    Add File

---
+++ Поиск строки по файлам.py
@@ -0,0 +1,30 @@
+# Скрипт проходится по папке с файлами и ищет заданную строку. Может быть полезно для быстрого поиска своих логов в списках победителей (если таких списков много)
+#
+# Достаточно ввести искомые значения и папку с файлами
+
+import os
+text = input("input text : ")
+path = input("path : ")
+# os.chdir(path)
+def getfiles(path):
+    f = 0
+    os.chdir(path)
+    files = os.listdir()
+    # print(files)
+    for file_name in files:
+        abs_path = os.path.abspath(file_name)
+        if os.path.isdir(abs_path):
+            getfiles(abs_path)
+        if os.path.isfile(abs_path):
+            f = open(file_name, "r")
+            if text in f.read():
+                f = 1
+                print(text + " found in ")
+                final_path = os.path.abspath(file_name)
+                print(final_path)
+                return True
+    if f == 1:
+        print(text + " not found! ")
+        return False
+
+getfiles(path)
